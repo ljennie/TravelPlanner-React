af4dcbc1ec8dcf97fa64cc9f01008bb382b23785
chuyi <chuyingl@umich.edu>
2019-03-20 03:12:19 -0400

    add  bootstrat button

--- frontEnd/travelplanner-web/src/components/HomeTab.js
+++ frontEnd/travelplanner-web/src/components/HomeTab.js
@@ -5,14 +5,12 @@ import { WrappedTravelMap } from "./TravelMap"
 class HomeTab extends Component {
     render() {
         return (
-            
             <WrappedTravelMap
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
-                loadingElement={<div style={{ height: `100%`, width: `100%` }} />}
-                containerElement={<div style={{ height: `600px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
+                loadingElement={<div style={{ height: 300, width: 300 }} />}
+                containerElement={<div style={{ height: 550 }} />}
+                mapElement={<div style={{ height:'80%', width:'100%' }} />}
             />
-           
             );
     }
 }
