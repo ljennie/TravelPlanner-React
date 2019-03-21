72027efb1f98c05454dc5f179955ae20e9123a09
dlxm455 <shijia.gemini@gmail.com>
2019-03-21 07:12:01 -0400

    temporarily add map2 and marker2 as the separate base components; add overview; add rightclock dropdown on marker

--- frontEnd/travelplanner-web/src/components/HomeTab.js
+++ frontEnd/travelplanner-web/src/components/HomeTab.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { WrappedTravelMap } from "./TravelMap"
+import { TravelOverview } from "./TravelOverview"
 
 
 class HomeTab extends Component {
     render() {
         return (
-            <WrappedTravelMap
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
-                loadingElement={<div style={{ height: 300, width: 300 }} />}
-                containerElement={<div style={{ height: 550 }} />}
-                mapElement={<div style={{ height:'80%', width:'100%' }} />}
-            />
+            <TravelOverview/>
             );
     }
 }
---
+++ frontEnd/travelplanner-web/src/constants.js
@@ -0,0 +1,10 @@
+export const API_ROOT="";
+export const TOKEN_KEY = "TOKEN_KEY";
+export const GEO_OPTIONS = {
+    enableHighAccuracy: true,
+    timeout: 27000,
+    maximumAge: 3600000
+};
+export const POS_KEY = "POS_KEY";
+export const AUTH_PREFIX = 'Bearer';
+export const LOC_SHAKE = 0.02;
