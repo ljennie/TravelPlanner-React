e8c0b6322084f3cbe318b4e8f419bb8deeeaeb62
dlxm455 <shijia.gemini@gmail.com>
2019-03-22 01:17:27 -0400

    finish generate paths with passing props

--- frontEnd/travelplanner-web/src/components/HomeTab.js
+++ frontEnd/travelplanner-web/src/components/HomeTab.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import { TravelOverview } from "./TravelOverview"
 
+import { TravelOverview } from "./TravelOverview"
+import { TestPage } from "./TestPage" // for testing
+
+import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
 
 class HomeTab extends Component {
     render() {
         return (
-            <TravelOverview/>
-            );
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/"  component={TravelOverview} />
+                    <Route path="/detail" component={TestPage} />
+
+                </Switch>
+            </BrowserRouter>
+        );
     }
 }
 
