d6919fb8472429453d5e76579bd924781bfdfcad
CompromisoHF <45577536+CompromisoHF@users.noreply.github.com>
2019-04-10 12:26:41 -0500

    Drag Bug fixed

--- frontEnd/travelplanner-web/src/components/SideTimeline.js
+++ frontEnd/travelplanner-web/src/components/SideTimeline.js
@@ -9,15 +9,15 @@ export default class SideTimeline extends React.Component {
         const planList = ((Days)=>{
             let list = new Array();
             for (var i=0;i<=Days.length;i++){
+                if (Days[i]===undefined) continue;
                 var innerstr;
                 innerstr = 'Day ' + (i+1);
                 list.push(<Timeline.Item classname = 'day-title' dot={<Icon type="pushpin" theme="twoTone"  style={{ fontSize: '16px' }}/>} >{'Day ' + (i+1)}</Timeline.Item>);
-                console.log(Days[i]);
-                if (Days[i]===undefined) continue;
+               // console.log(Days[i]);    
                 for (var j=0;j<=Days[i].length;j++){
                     if (Days[i][j]===undefined) continue;
                     innerstr = (j+1) + '. ' + Days[i][j].name ;
-                    console.log(Days[i]);
+                   // console.log(Days[i]);
                     list.push(<Timeline.Item color="green" classname = 'intra-list'>{innerstr}</Timeline.Item>);
                 }
             }   
